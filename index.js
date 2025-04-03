b7bac35a01b032f26c0f7d513fcd5d172fb4e546
b.khayrullin <b.khayrullin@tbank.ru>
2025-04-03 12:37:25 +0300

    pass arguments individually

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ const stylelint = require("stylelint");
 const ruleName = "aditayvm/at-rule-no-children";
 
 const messages = stylelint.utils.ruleMessages(ruleName, ({
-  unexpected: (block, rule) => `Unexpected rule "${block}" inside at-rule "${rule}".`,
+    unexpected: (block, rule) => `Unexpected rule "${block}" inside at-rule "${rule}".`,
 }));
 
-module.exports = stylelint.createPlugin(ruleName, function(options = "") {
-  return function(root, result) {
-    var validOptions = stylelint.utils.validateOptions(
-      ruleName,
-      result,
-      options,
-    );
+module.exports = stylelint.createPlugin(ruleName, function (options = "") {
+    return function (root, result) {
+        var validOptions = stylelint.utils.validateOptions(
+            ruleName,
+            result,
+            {actual: options},
+        );
 
-    if (!validOptions) {
-      return;
-    }
-
-    const params = new RegExp(`(${((options || {}).ignore || []).join("|")})\\\(`);
-
-    root.walkAtRules(function(statement) {
-      (statement.nodes || []).forEach(node => {
-        if ((options || {}).ignore) { // ignore list
-          if (options.ignore.includes(node.parent.name)) return; // @foo
-          if (params.test(node.parent.params)) return; // @include foo
+        if (!validOptions) {
+            return;
         }
-        if (node.type !== "rule") return; // allow non-rules
-        if (node.selector.match(/^[^.#]?(\d+%|from|to)/)) return; // allow 90% / from / to
 
-        stylelint.utils.report({
-          ruleName,
-          result,
-          node,
-          message: messages.unexpected(node.selector, statement.name),
+        const params = new RegExp(`(${((options || {}).ignore || []).join("|")})\\\(`);
+
+        root.walkAtRules(function (statement) {
+            (statement.nodes || []).forEach(node => {
+                if ((options || {}).ignore) { // ignore list
+                    if (options.ignore.includes(node.parent.name)) return; // @foo
+                    if (params.test(node.parent.params)) return; // @include foo
+                }
+                if (node.type !== "rule") return; // allow non-rules
+                if (node.selector.match(/^[^.#]?(\d+%|from|to)/)) return; // allow 90% / from / to
+
+                stylelint.utils.report({
+                    ruleName,
+                    result,
+                    node,
+                    message: messages.unexpected(node.selector, statement.name),
+                });
+            });
         });
-      });
-    });
-  };
+    };
 });
