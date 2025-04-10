4dc24f3ad7a84cf7b189ecbd4cfe3926efc4a7f8
Aditya Mukherjee <github@adityamukherjee.com>
2025-04-10 22:27:11 +1000

    Remove extra spaces everywhere

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const messages = stylelint.utils.ruleMessages(ruleName, ({
   unexpected: (block, rule) => `Unexpected rule "${block}" inside at-rule "${rule}".`,
 }));
 
-module.exports = stylelint.createPlugin(ruleName, function (options = "") {
-  return function (root, result) {
+module.exports = stylelint.createPlugin(ruleName, function(options = "") {
+  return function(root, result) {
     var validOptions = stylelint.utils.validateOptions(
-        ruleName,
-        result,
-        options,
+      ruleName,
+      result,
+      options,
     );
 
     if (!validOptions) {
       return;
     }
 
-    const params = new RegExp(`(${((options || {}).ignore || []).join("|")})\\\(`)
+    const params = new RegExp(`(${((options || {}).ignore || []).join("|")})\\\(`);
 
-    root.walkAtRules(function (statement) {
+    root.walkAtRules(function(statement){
       (statement.nodes || []).forEach(node => {
         if ((options || {}).ignore) { // ignore list
           if (options.ignore.includes(node.parent.name)) return; // @foo
